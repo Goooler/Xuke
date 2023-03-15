021d446f4f8bfaa0b6d65c966c8bd9f2d25b3856
Goooler <wangzongler@gmail.com>
2023-03-15 18:09:20 +0800

    No extra config for ktlint

--- plugin-build/build.gradle.kts
+++ plugin-build/build.gradle.kts
@@ -3,20 +3,3 @@ plugins {
     alias(libs.plugins.ktlint)
     alias(libs.plugins.pluginPublish)
 }
-
-allprojects {
-    pluginManager.apply(rootProject.libs.plugins.ktlint.get().pluginId)
-
-    ktlint {
-        debug.set(false)
-        verbose.set(true)
-        android.set(false)
-        outputToConsole.set(true)
-        ignoreFailures.set(false)
-        enableExperimentalRules.set(true)
-        filter {
-            exclude("**/generated/**")
-            include("**/kotlin/**")
-        }
-    }
-}
